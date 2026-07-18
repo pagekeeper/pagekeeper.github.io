6dda109ce4a91158e4a7a0f66359eddd9c76859f
Juan Jose de Haro <jjdeharo@gmail.com>
2026-07-18 12:50:12 +0200

    Evita que el historial cubra la lectura

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker: guarda en caché la aplicación para que funcione sin
 // conexión. Las peticiones al servidor WebDAV nunca se cachean.
 
-const CACHE = 'pagekeeper-v23';
+const CACHE = 'pagekeeper-v24';
 
 const RECURSOS = [
   '.',
