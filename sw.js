cd0252254ac0eadec1e3e8f23a16f86f210db9d4
Juan Jose de Haro <jjdeharo@gmail.com>
2026-07-18 20:51:51 +0200

    Reordena las acciones de libros en móvil

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker: guarda en caché la aplicación para que funcione sin
 // conexión. Las peticiones al servidor WebDAV nunca se cachean.
 
-const CACHE = 'pagekeeper-v37';
+const CACHE = 'pagekeeper-v38';
 
 const RECURSOS = [
   '.',
